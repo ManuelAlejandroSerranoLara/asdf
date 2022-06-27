5c76ddae3c082022e794dcc4384f732f272bb5d0
ManoTheHand <masl46978@gmail.com>
2022-06-26 23:09:58 -0500

    Primer Commit.

---
+++ app/src/main/java/com/example/proyecto_respaldo/Actividad1Sesion.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_respaldo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Actividad1Sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad1_sesion);
+    }
+}
